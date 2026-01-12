f4d660d2bae5a88b17052866e88db3e08c3ecd4c
inahym196 <subihym@gmail.com>
2026-01-13 01:00:49 +0900

    applyLogをdeductionにrename
    適用結果ではなく推論結果という内容に変える前段階

--- deducer.go
+++ deducer.go
@@ -1,5 +1,19 @@
 package picrosssolver
 
+import "fmt"
+
+type deduction struct {
+	ruleName string
+	hints    []int
+	lineRef  lineRef
+	before   []Cell
+	after    []Cell
+}
+
+func (deduction deduction) String() string {
+	return fmt.Sprintf("%s %s %v %v -> %v", deduction.ruleName, deduction.lineRef, deduction.hints, deduction.before, deduction.after)
+}
+
 type deducer struct {
 	rules []Rule
 }
--- solver.go
+++ solver.go
@@ -1,22 +1,9 @@
 package picrosssolver
 
 import (
-	"fmt"
 	"reflect"
 	"slices"
 )
-
-type applyLog struct {
-	ruleName string
-	hints    []int
-	lineRef  lineRef
-	before   []Cell
-	after    []Cell
-}
-
-func (log applyLog) String() string {
-	return fmt.Sprintf("%s %s %v %v -> %v", log.ruleName, log.lineRef, log.hints, log.before, log.after)
-}
 
 type Solver struct {
 	deducer deducer
@@ -26,19 +13,19 @@ func NewSolver() Solver {
 	return Solver{newDeducer()}
 }
 
-func (s Solver) ApplyLine(acc lineAccessor, hints []int) (changed bool, logs []applyLog) {
+func (s Solver) ApplyLine(acc lineAccessor, hints []int) (changed bool, deductions []deduction) {
 	// TODO: lineごとにrulesを適用し、最後にApplyすればApply頻度を下げられる
 	for _, rule := range s.deducer.rules {
 		before := acc.Cells()
 		if slices.Index(before, CellUndetermined) == -1 {
-			return changed, logs
+			return changed, deductions
 		}
 		line := lineView{slices.Clone(before), slices.Clone(hints)}
 		updated := rule.Deduce(line)
 		if updated != nil && !reflect.DeepEqual(before, updated) {
 			changed = true
 			acc.Update(updated)
-			logs = append(logs, applyLog{
+			deductions = append(deductions, deduction{
 				ruleName: rule.Name(),
 				hints:    hints,
 				lineRef:  acc.Ref(),
@@ -47,29 +34,29 @@ func (s Solver) ApplyLine(acc lineAccessor, hints []int) (changed bool, logs []a
 			})
 		}
 	}
-	return changed, logs
+	return changed, deductions
 }
 
-func (s Solver) ApplyOnce(game Game) (board Board, changed bool, logs []applyLog) {
+func (s Solver) ApplyOnce(game Game) (board Board, changed bool, deductions []deduction) {
 
 	board = slices.Clone(game.board)
 	for i := range game.rowHints {
 		acc := lineAccessor{&game.board, lineRef{lineKindRow, i}}
-		changedLine, lineLogs := s.ApplyLine(acc, game.rowHints[i])
+		changedLine, lineDeds := s.ApplyLine(acc, game.rowHints[i])
 		if changedLine {
-			logs = append(logs, lineLogs...)
+			deductions = append(deductions, lineDeds...)
 			changed = true
 		}
 	}
 	for i := range game.colHints {
 		acc := lineAccessor{&game.board, lineRef{lineKindColumn, i}}
-		changedLine, lineLogs := s.ApplyLine(acc, game.colHints[i])
+		changedLine, lineDeds := s.ApplyLine(acc, game.colHints[i])
 		if changedLine {
-			logs = append(logs, lineLogs...)
+			deductions = append(deductions, lineDeds...)
 			changed = true
 		}
 	}
-	return board, changed, logs
+	return board, changed, deductions
 }
 
 func (s Solver) checkComplete(board Board) bool {
@@ -81,18 +68,18 @@ func (s Solver) checkComplete(board Board) bool {
 	return true
 }
 
-func (s Solver) ApplyMany(game Game) (Board, int, []applyLog) {
-	var logs []applyLog
+func (s Solver) ApplyMany(game Game) (Board, int, []deduction) {
+	var deds []deduction
 	board := DeepCopyBoard(game.board)
 	n := 0
 	for !s.checkComplete(board) {
 		n++
-		deduced, changedLine, lineLogs := s.ApplyOnce(game)
+		deduced, changedLine, lineDeds := s.ApplyOnce(game)
 		if !changedLine {
-			return board, n, logs
+			return board, n, deds
 		}
 		board = deduced
-		logs = append(logs, lineLogs...)
+		deds = append(deds, lineDeds...)
 	}
-	return board, n, logs
+	return board, n, deds
 }
