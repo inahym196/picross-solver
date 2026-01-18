13e150a145d9d55c1a731668562823953c0cfbdc
inahym196 <subihym@gmail.com>
2026-01-19 00:42:48 +0900

    変数名の微修正

--- pkg/solver/internal/rules/edge_expansion_rule.go
+++ pkg/solver/internal/rules/edge_expansion_rule.go
@@ -14,31 +14,31 @@ func (r EdgeExpansionRule) Name() string {
 	return "EdgeExpansionRule"
 }
 
-func (r EdgeExpansionRule) expand(cells []game.Cell, hint int, idx func(i, n int) int) bool {
+func (r EdgeExpansionRule) expand(cells []game.Cell, hint int, indexer func(i, n int) int) bool {
 	n := len(cells)
-	if n < hint || cells[idx(0, n)] != game.CellBlack {
+
+	if n < hint || cells[indexer(0, n)] != game.CellBlack {
 		return false
 	}
 
 	for i := range hint {
-		cells[idx(i, n)] = game.CellBlack
+		cells[indexer(i, n)] = game.CellBlack
 	}
 	if hint < n {
-		cells[idx(hint, n)] = game.CellWhite
+		cells[indexer(hint, n)] = game.CellWhite
 	}
 	return true
 }
 
 func (r EdgeExpansionRule) Deduce(line line.Line) []game.Cell {
 	cells := slices.Clone(line.Cells)
-
 	trim := trimWhite(cells)
 
-	leftIndex := func(i, _ int) int { return i }
-	expand1 := r.expand(trim, line.Hints[0], leftIndex)
+	leftIndexer := func(i, _ int) int { return i }
+	expand1 := r.expand(trim, line.Hints[0], leftIndexer)
 
-	rightIndex := func(i, n int) int { return n - 1 - i }
-	expand2 := r.expand(trim, line.Hints[len(line.Hints)-1], rightIndex)
+	rightIndexer := func(i, n int) int { return n - 1 - i }
+	expand2 := r.expand(trim, line.Hints[len(line.Hints)-1], rightIndexer)
 
 	if !expand1 && !expand2 {
 		return nil
