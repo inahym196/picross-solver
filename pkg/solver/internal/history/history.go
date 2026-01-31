1b71c9d144c3802281d1726e5f3cd72cee52f529
inahym196 <subihym@gmail.com>
2026-01-31 15:23:35 +0900

    historyの実装

---
+++ pkg/solver/internal/history/history.go
@@ -0,0 +1,26 @@
+package history
+
+import "github.com/inahym196/picross-solver/pkg/solver/internal/domain"
+
+type Step struct {
+	RuleName string
+	Domain   domain.LineDomain
+}
+
+type History struct {
+	steps []Step
+}
+
+func NewHistory() *History { return &History{} }
+
+func (h *History) Append(steps ...Step) {
+	h.steps = append(h.steps, steps...)
+}
+
+func (h *History) Merge(other History) {
+	h.steps = append(h.steps, other.steps...)
+}
+
+func (h *History) IsEmpty() bool { return len(h.steps) == 0 }
+
+func (h *History) Last() Step { return h.steps[len(h.steps)-1] }
